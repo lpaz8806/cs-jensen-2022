f361b0d61b3d6cdff17e15a44dadc2a86ee1ec9c
Luis Paz <lpaz8806@gmail.com>
2022-09-14 10:53:26 +0200

    recursion

--- L04Recursion/Examples.cs
+++ L04Recursion/Examples.cs
@@ -2,5 +2,63 @@ namespace Recursion;
 
 partial class Program
 {
+    ///<summary>
+    /// 1  2  3  4 
+    /// 0  1  2  3  4  5
+    /// 0, 1, 1, 2, 3, 5, 8, 13...
+    /// Fib[1] = 0
+    /// Fib[2] = 1
+    /// Fib[n] = Fib[n-1] + Fib[n-2]
+    /// </summary>
+    /// <param name="n"></param>
+    /// <returns></returns>
+    static int Fibonacci(int n)
+    {
+        if (n <= 0)
+            throw new ArgumentException();
+        
+        if (n == 1)
+            return 0;
+        
+        if (n == 2)
+            return 1;
+
+        return Fibonacci(n - 1) + Fibonacci(n - 2);
+    }
     
+    static void HanoiMove(int n, int start, int end, int aux)
+    {
+        if (n == 1)
+        {
+            Console.WriteLine($"Move from {start} to {end}");
+            return;
+        }
+        
+        HanoiMove(n - 1, start, aux, end);
+        HanoiMove(1, start, end, aux);
+        HanoiMove(n - 1, aux, end, start);
+    }
+    
+    static int MaxInArray(int[] numbers)
+    {
+        if (numbers.Length == 0)
+            throw new Exception();
+        
+        if (numbers.Length == 1)
+            return numbers[0];
+        
+        var current = numbers[0];
+        var maxRest = MaxInArray(Rest(numbers));
+        return current > maxRest ? current : maxRest;
+    }
+    
+    static int[] Rest(int[] arr)
+    {
+        int[] rest = new int[arr.Length - 1];
+        
+        for (int i = 0; i < rest.Length; i++)
+            rest[i] = arr[i + 1];
+        
+        return rest;
+    }
 }
--- L04Recursion/Program.cs
+++ L04Recursion/Program.cs
@@ -4,5 +4,8 @@ partial class Program
 {
     static void Main(string[] args)
     {
+        int[] numbers = { 1, 3, 13, 5, 69, 11 };
+        var max = MaxInArray(numbers);
+        Console.WriteLine(max);
     }
 }
