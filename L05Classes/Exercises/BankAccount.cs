7e3ca1c6dcfe52d1d665fd456ab648293eedc9b6
Luis Paz <lpaz8806@gmail.com>
2022-09-19 17:05:12 +0200

    Update BankAccount.cs

--- L05Classes/Exercises/BankAccount.cs
+++ L05Classes/Exercises/BankAccount.cs
@@ -8,7 +8,41 @@ namespace L05Classes;
  *   When the amount of money to widthrdaw is greater than the
  *   saldo the operation must fail.
  * - Transfer the specified amount to another account
- * - Query the Saldo of the account
- * - Query the Name of the owner
+ * - Ask for the Saldo of the account
+ * - Ask for the Name of the owner
 */
+
+class BankAccount
+{
+    public string Owner { get; }
+    public decimal Saldo { get; private set; }
+    public BankAccount(string owner, decimal initialSaldo)
+    {
+        Owner = owner;
+        Saldo = initialSaldo;
+    }
+
+    public void Deposit(decimal amount)
+    {
+        if (amount <= 0)
+            throw new ArgumentException("");
+        
+        Saldo += amount;
+    }
+    
+    public void Withdraw(decimal amount)
+    {
+        if (amount > Saldo)
+            throw new InvalidOperationException(
+                "You don't have enough fund, you poor soul");
+        
+        Saldo -= amount;
+    }
+    public void Transfer(decimal amount, BankAccount other)
+    {
+        Withdraw(amount);
+        other.Deposit(amount);
+    }
+    
+}
 
