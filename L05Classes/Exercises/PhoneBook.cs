0e10debf4c9483e999edd03e9037bf736f0c2c24
Luis Paz <lpaz8806@gmail.com>
2022-09-21 09:16:04 +0200

    Create PhoneBook.cs

---
+++ L05Classes/Exercises/PhoneBook.cs
@@ -0,0 +1,30 @@
+namespace L05Classes;
+/// <summary>
+/// You can implement a PhoneBook that can hold
+/// up to 10 name-phoneNumber pairs
+///
+/// Operations to support:
+/// - Count: Retrieves how many numbers are stored
+/// - Add(string name, string phoneNumber)
+///     Adds the specified name-phoneNumber pair to the phonebook.
+/// - Remove(string name): removes the pair which name is "name"
+/// - Print(): 
+///     Displays in the console all the name-number pairs 
+///     stored in the phone book. One pair per line.
+///
+/// Notice that the specification is very "open". You can
+/// decide how to handle the edge cases. For example,
+/// what happens when the phonebook is full and you try to
+/// add another pair. What happens when you try to add a pair
+/// that which name already exists. What happens when you
+/// try to delete a pair which name does not exist and so on.
+///
+/// There is no "correct" answer. It is all up to you to decide
+/// how YOUR phonebook will work :)
+///
+/// Have Fun!
+/// </summary>
+public class PhoneBook
+{
+    
+}
