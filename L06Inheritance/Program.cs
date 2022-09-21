cf2b45e35f83f86108da04ad32fe9ed1d903bfc3
Luis Paz <lpaz8806@gmail.com>
2022-09-21 08:39:25 +0200

    Update Program.cs

--- L06Inheritance/Program.cs
+++ L06Inheritance/Program.cs
@@ -1,3 +1,15 @@
 ﻿// See https://aka.ms/new-console-template for more information
 
+
+/*
+Inheritance
+Polymorfism
+virtual och override
+Abstrakta klasser
+Casting
+Object superclass
+Operators is and as
+Sealed
+ */
+
 Console.WriteLine("Hello, World!");
