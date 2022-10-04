a4fc83e7340b770817254b2f547f12b1e6211c22
Luis Paz <lpaz8806@gmail.com>
2022-10-04 08:21:10 +0200

    Update Program.cs

--- L05Classes/Program.cs
+++ L05Classes/Program.cs
@@ -4,9 +4,5 @@ class Program
 {
     static void Main()
     {
-        var parser = new RationalParser();
-        var str = Console.ReadLine() ?? String.Empty;
-        var r1 = parser.Parse(str);
-        Console.WriteLine(r1);
     }
 }
