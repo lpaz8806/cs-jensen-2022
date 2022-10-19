a564f31207dcd98628a0ccff761f7d5b1b0bb98d
Luis Paz <lpaz8806@gmail.com>
2022-10-19 11:14:19 +0200

    handledning 1019

--- L09DataStructures/Queue/LinkedQueue.cs
+++ L09DataStructures/Queue/LinkedQueue.cs
@@ -5,39 +5,39 @@ namespace L09DataStructures.Queue;
 
 public class LinkedQueue<T> : IQueue<T>, IEnumerable<T>
 {
-    private MyLinkedNode<T> head;
-    private MyLinkedNode<T> tail;
+    private MyLinkedNode<T> first;
+    private MyLinkedNode<T> last;
 
 
     public LinkedQueue()
     {
-        tail = new MyLinkedNode<T>(default);
-        head = new MyLinkedNode<T>(default, tail);
+        last = new MyLinkedNode<T>(default);
+        first = new MyLinkedNode<T>(default, last);
     }
     
     
     public void Enqueue(T item)
     {
-        tail.Value = item;
-        tail.Next = new MyLinkedNode<T>(default);
-        tail = tail.Next;
+        last.Value = item;
+        last.Next = new MyLinkedNode<T>(default);
+        last = last.Next;
     }
 
     public T Dequeue()
     {
         var front = Front;
-        head = head.Next;
+        first = first.Next;
         return front;
     }
 
-    public T Front => !IsEmpty ? head.Next.Value : throw new InvalidOperationException("Queue is empty");
-    public bool IsEmpty => head.Next == tail;
+    public T Front => !IsEmpty ? first.Next.Value : throw new InvalidOperationException("Queue is empty");
+    public bool IsEmpty => first.Next == last;
 
     IEnumerator IEnumerable.GetEnumerator() => GetEnumerator();
 
     public IEnumerator<T> GetEnumerator()
     {
-        for (var cursor = head.Next; cursor.Next != null; cursor = cursor.Next)
+        for (var cursor = first.Next; cursor.Next != null; cursor = cursor.Next)
         {
             yield return cursor.Value;
         }
